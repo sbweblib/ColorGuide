1103f7b2776dfa6d30a94e6017f7cf543d1aae6e
Socialbox Dev <y-yamamoto@sb-i.jp>
2019-02-01 19:39:11 +0900

    2019-02-01: Change the function hexs name to function toHex,

--- main.js
+++ main.js
@@ -15,10 +15,10 @@
     };
     var n;
 
-    function hexs(col){
+    function toHex(col){
         return '#'+col.match(/\d+/g).map(function(a){return ("0"+parseInt(a).toString(16)).slice(-2)}).join("");
     }
-    
+
     function change(v,type){
         if(type=="red"){
             bodyset.style.backgroundColor='rgb('+v+','+color['g']+','+color['b']+')';
@@ -28,7 +28,7 @@
             bodyset.style.backgroundColor='rgb('+color['r']+','+v+','+color['b']+')';
         }
 
-      hex.textContent=hexs('rgb('+color['r']+','+color['g']+','+color['b']+')');
+      hex.textContent=toHex('rgb('+color['r']+','+color['g']+','+color['b']+')');
     }
 
     red.addEventListener('input',e=>{
